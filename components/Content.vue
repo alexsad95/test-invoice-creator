3f04c30997b85868ca9fe2256aa94386dd7753c6
alexsad95 <aleksandrsadchikov.17@gmail.com>
2025-09-20 16:54:50 +0300

    refactor: renamed to NewInvoice

--- components/Content.vue
+++ components/Content.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import NewInvoiceForm from '~/components/NewInvoiceForm.vue';
+import NewInvoice from '~/components/NewInvoice.vue';
 import PDFPreview from '~/components/PDFPreview.vue';
 </script>
 
@@ -7,7 +7,7 @@ import PDFPreview from '~/components/PDFPreview.vue';
   <main class="bg-gray-100 flex-1 flex items-center justify-center">
     <div class="container mx-auto px-4">
       <div class="flex flex-col lg:flex-row max-w-8xl mx-auto gap-4">
-        <NewInvoiceForm />
+        <NewInvoice />
         <PDFPreview />
       </div>
     </div>
